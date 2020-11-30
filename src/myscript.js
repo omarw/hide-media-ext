461cff7d1aefb0d54aa908b2a3756283afad1f6d
Omar W <omar.wheatley@gmail.com>
2020-11-30 17:35:27 -0500

    Init

---
+++ src/myscript.js
@@ -0,0 +1,17 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	// No tabs or host permissions needed!
+	chrome.browserAction.setBadgeBackgroundColor({ color: [255, 255, 0, 255] });
+	chrome.browserAction.setBadgeText({text: "...",tabId: tab.id});
+	chrome.tabs.executeScript(null, { file: "jquery-2.1.3.min.js", allFrames:true }, function() {		
+		chrome.tabs.executeScript(null, { file: "contentscript.min.js", allFrames:true }, function(){
+			if (!chrome.runtime.lastError) {
+				chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
+				chrome.browserAction.setBadgeText({text: "_",tabId: tab.id});
+			}else{
+				chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+				chrome.browserAction.setBadgeText({text: "x",tabId: tab.id});
+				console.log(chrome.runtime.lastError.message);
+			}
+		});
+	});	
+});
